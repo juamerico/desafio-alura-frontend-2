725dd52fedc73bfb620945dd7377a2cdbe2483c4
Juliana Americo <juu.americo@gmail.com>
2021-12-15 22:24:14 -0300

    Correção no grid, reatribuindo responsabilidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CodeEditorModule } from './code-editor/code-editor.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     HeaderModule,
+    CodeEditorModule,
     NavigationMenuModule,
     HttpClientModule
   ],
