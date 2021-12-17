be679395787b844b2ab751be939a1c3309d30ac9
Juliana Americo <juu.americo@gmail.com>
2021-12-16 21:30:01 -0300

    Parte visual do input de códigos criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { CodeEditorComponent } from './code-editor/code-editor.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "code-editor"
+  },
+  {
+    path: "code-editor",
+    component: CodeEditorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
