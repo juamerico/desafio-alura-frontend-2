646e8f78b257e8c3caba4970bdf297f6731d8c27
Juliana Americo <juu.americo@gmail.com>
2021-12-25 17:21:43 -0300

    Opacidade e routerLink

--- src/app/shared/navigation-menu/navigation-menu.module.ts
+++ src/app/shared/navigation-menu/navigation-menu.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationMenuComponent } from './navigation-menu.component';
+import { AppRoutingModule } from "../../app-routing.module"
 
 
 
@@ -9,7 +10,8 @@ import { NavigationMenuComponent } from './navigation-menu.component';
     NavigationMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [
     NavigationMenuComponent
