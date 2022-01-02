16a551e0dad2bbe87515179b339692f8a3c48b34
Juliana Americo <juu.americo@gmail.com>
2022-01-01 21:05:55 -0300

    Correção no overflow do body e inclusão do code input e code output dentro dos parent components

--- src/app/code-editor/code-editor.module.ts
+++ src/app/code-editor/code-editor.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeEditorComponent } from './code-editor.component';
-import { CodeInputComponent } from './code-input/code-input.component';
-import { CodeMenuComponent } from './code-menu/code-menu.component';
 
 
 
 @NgModule({
   declarations: [
-    CodeEditorComponent,
-    CodeInputComponent,
-    CodeMenuComponent
+    CodeEditorComponent
   ],
   imports: [
     CommonModule
