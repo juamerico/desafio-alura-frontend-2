fdf7981f679d6714bbac6800503402365e7472cc
Juliana Americo <juu.americo@gmail.com>
2022-01-09 15:33:48 -0300

    Seletor de cor funcionando

--- src/app/code-editor/code-editor.component.ts
+++ src/app/code-editor/code-editor.component.ts
@@ -1,5 +1,5 @@
 import { HighlightJsService } from './../services/highlight-js.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -7,19 +7,18 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './code-editor.component.html',
   styleUrls: ['./code-editor.component.scss']
 })
-export class CodeEditorComponent implements OnInit {
-  public code = `.body {
-    color: red;
-  }`
+export class CodeEditorComponent {
+
+  public color: string = "#6BD1FF"
 
   constructor(private hljs: HighlightJsService, private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
-
-  highlight(event: Event) {
+  public highlight(event: Event): void {
     event.preventDefault()
     this.hljs.highlighText()
   }
 
+  public changeColor(event: Event): void {
+    this.color = (<HTMLInputElement>event.target).value
+  }
 }
